6d79df90bfbadd6b5644c15a9e7c5a59396f8933
Andrew <andrewbryson12@gmail.com>
2024-11-14 16:38:32 +0100

    init

---
+++ src/lib/List.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+   let list = [...Array(8)].map((_, i) => i)
+   let selected: number = $state(0)
+
+   const selectMe = (index: number) => {
+      selected = index
+   }
+</script>
+
+<div>
+   {#each list as n, i (n)}
+      <button onclick={() => selectMe(i)}>{i}</button>
+   {/each}
+</div>
